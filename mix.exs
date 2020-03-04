b321f431d7371a1c5aa72e211b1cf8af00b7307d
Eugene Korneev <ekorneeff@gmail.com>
2020-03-04 20:02:05 +0700

    add ex_doc

--- mix.exs
+++ mix.exs
@@ -30,7 +30,8 @@ defmodule Dota2ex.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.6"},
-      {:jason, "~> 1.1"}
+      {:jason, "~> 1.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
